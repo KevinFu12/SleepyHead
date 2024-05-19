67bf85d56f30c6f2be8a39e9007284f64cb46932
unknown <kevinfujianto124@gmail.com>
2024-05-19 11:18:55 +0700

    initial project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SleepyHead_App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
